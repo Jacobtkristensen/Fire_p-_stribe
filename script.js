6eaff7d8778fdf758229fdcedc2fc92a1c7d2d6b
Jacob Kristensen <jacobtkristensen@gmail.com>
2024-03-03 21:35:46 +0100

    structure js file

--- script.js
+++ script.js
@@ -2,6 +2,12 @@ import confetti from "https://esm.run/canvas-confetti@1";
 
 window.addEventListener("load", start);
 
+let currentPlayer = 1;
+
+const modal = document.querySelector(".modal");
+
+
+// >>>>>>>>>> MODEL <<<<<<<<<<  
 const model = [
     [0, 0, 0, 0, 0, 0, 0],
     [0, 0, 0, 0, 0, 0, 0],
@@ -11,51 +17,10 @@ const model = [
     [0, 0, 0, 0, 0, 0, 0],
 ];
 
-
-let currentPlayer = 1;
-
-const modal = document.querySelector(".modal");
-
-
-function start() {
+function writeToCell(row, col, value) {
+    model[row][col] = value;
+    console.table(model);
     displayBoard();
-
-    const board = document.querySelector("#board");
-    board.addEventListener("click", boardClicked);
-
-}
-function boardClicked(event) {
-    console.log("Board clicked");
-
-    const cell = event.target;
-
-    if (cell.classList.contains("cell")) {
-        const col = cell.dataset.col;
-        selectColumn(col);
-    }
-    checkForWinner();
-}
-
-
-function displayBoard() {
-    for (let row = 0; row < model.length; row++) {
-        for (let col = 0; col < model[row].length; col++) {
-            const value = model[row][col];
-            const cell = document.querySelector(`[data-row="${row}"][data-col="${col}"]`);
-
-            switch (value) {
-                case 0:
-                    cell.style.background = "white";
-                    break;
-                case 1:
-                    cell.style.background = "blue";
-                    break;
-                case 2: cell.style.background = "red";
-
-                    break;
-            }
-        }
-    }
 }
 
 function checkForWinner() {
@@ -113,11 +78,65 @@ function checkForWinner() {
         }
     }
 }
+// >>>>>>>>>> VIEW <<<<<<<<<< 
+function displayBoard() {
+    for (let row = 0; row < model.length; row++) {
+        for (let col = 0; col < model[row].length; col++) {
+            const value = model[row][col];
+            const cell = document.querySelector(`[data-row="${row}"][data-col="${col}"]`);
 
-function writeToCell(row, col, value) {
-    model[row][col] = value;
-    console.table(model);
+            switch (value) {
+                case 0:
+                    cell.style.background = "white";
+                    break;
+                case 1:
+                    cell.style.background = "blue";
+                    break;
+                case 2: cell.style.background = "red";
+
+                    break;
+            }
+        }
+    }
+}
+
+function openModal() {
+    modal.style.display = "block";
+    const modalText = document.querySelector(".modal-content");
+    modalText.innerHTML = `<h2>Congratulations!</h2>` + "<br>" +
+        `<button onclick="location.reload()">Play again</button>`;
+}
+
+function Confetti() {
+    if (checkForWinner) {
+        confetti({
+            particleCount: 1000,
+            spread: 300,
+            origin: { y: 0.45 }
+        })
+    }
+}
+
+// >>>>>>>>>> CONTROLLER <<<<<<<<<<
+
+function start() {
+    console.log("JS loaded");
     displayBoard();
+
+    const board = document.querySelector("#board");
+    board.addEventListener("click", boardClicked);
+
+}
+function boardClicked(event) {
+    console.log("Board clicked");
+
+    const cell = event.target;
+
+    if (cell.classList.contains("cell")) {
+        const col = cell.dataset.col;
+        selectColumn(col);
+    }
+    checkForWinner();
 }
 
 function selectColumn(col) {
@@ -134,22 +153,4 @@ function selectColumn(col) {
             return
         }
     }
-}
-
-
-function Confetti() {
-    if (checkForWinner) {
-        confetti({
-            particleCount: 1000,
-            spread: 300,
-            origin: { y: 0.45 }
-        })
-    }
-}
-
-function openModal() {
-    modal.style.display = "block";
-    const modalText = document.querySelector(".modal-content");
-    modalText.innerHTML = `<h2>Congratulations!</h2>` + "<br>" +
-     `<button onclick="location.reload()">Play again</button>`;
 }
